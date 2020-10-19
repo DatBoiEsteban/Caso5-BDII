3750c27a640ad7f0f83913c53fed04797d191ba0
Esteban Mata <esteban1113001@gmail.com>
2020-10-19 16:10:10 -0600

    Cambio en los queries de ES

--- elasticsearch/connection.ts
+++ elasticsearch/connection.ts
@@ -4,17 +4,57 @@ import { Client } from '@elastic/elasticsearch';
 const es = new Client({ node: 'http://localhost:9200' });
 
 /** Funcion para obtener todos los hashtags */
-export const getHashtags = async (): Promise<any[]> => {
+export const getHashtags = async (
+  inicio: number,
+  fin: number
+): Promise<any[]> => {
   // Ejecuta la peticion a elastic
   const result = await es.search({
     index: 'videos',
     body: {
+      size: 0,
       aggs: {
-        duplicate_aggs: {
+        hashtags_count: {
           terms: {
             field: 'description.keyword',
-            min_doc_count: 1,
-            size: 100,
+            size: 10000,
+          },
+        },
+        stats: {
+          stats_bucket: {
+            buckets_path: 'hashtags_count._count',
+          },
+        },
+      },
+    },
+  });
+
+  // Objeto que tiene los stats de min, max y demas de el query anterior
+  const stats = result.body.aggregations.stats;
+
+  // numero que nos da la distribuicion de las palabras relativo a las repeticiones
+  const modulo = Math.floor((stats.max - stats.min) / 10);
+
+  // Ejecuta el query que devuelve solo las palabras que deben ser utilizadas para las demas busquedas
+  const resultverdad = await es.search({
+    index: 'videos',
+    body: {
+      size: 0,
+      aggs: {
+        hashtags_count: {
+          terms: {
+            field: 'description.keyword',
+            size: 10000,
+          },
+          aggs: {
+            filto: {
+              bucket_selector: {
+                buckets_path: {
+                  total: '_count',
+                },
+                script: `params.total % ${modulo} >= ${inicio} && params.total % ${modulo} <= ${fin}`,
+              },
+            },
           },
         },
       },
@@ -22,5 +62,5 @@ export const getHashtags = async (): Promise<any[]> => {
   });
 
   // Y retorna los resultados
-  return result.body.aggregations.duplicate_aggs.buckets;
+  return resultverdad.body.aggregations.hashtags_count.buckets;
 };
